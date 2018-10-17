776f73ffd79de84170e94450019a92096fea2770
Samuel Elgozi <samu.elgozi@gmail.com>
2018-10-17 13:45:33 +0300

    Date type validator ready

---
+++ src/types/date.js
@@ -0,0 +1,26 @@
+module.exports = {
+	validateType(value) {
+		if (value === null) return false;
+
+		const d = new Date(value);
+		return d instanceof Date && !isNaN(d);
+	},
+
+	validateSchema(schemaObject) {
+		const allowedProps = ['type', 'required'];
+
+		for (let prop in schemaObject) {
+			// Check if the property is allowed.
+			if (!allowedProps.includes(prop)) {
+				throw Error(`Unknown property "${prop}"`);
+			}
+
+			// Verify that 'required' is a Boolean.
+			if (prop === 'required') {
+				if (typeof schemaObject.required !== 'boolean') {
+					throw Error('Property named "required" should be of type boolean');
+				}
+			}
+		}
+	}
+};
---
+++ tests/types/date.test.js
@@ -0,0 +1,45 @@
+const dateValidator = require('../../src/types/date');
+
+test('Only valid dates pass the "validateType" method', () => {
+	// Should pass
+	expect(dateValidator.validateType('04 Dec 1995 00:12:00 GMT')).toEqual(true);
+	expect(dateValidator.validateType(Date.now())).toEqual(true);
+
+	// Should fail
+	expect(dateValidator.validateType('04 Dud 1995 00:12:00 GMT')).toEqual(false);
+	expect(dateValidator.validateType('42 Dec 1995 00:12:00 GMT')).toEqual(false);
+	expect(dateValidator.validateType(function() {})).toEqual(false);
+	expect(dateValidator.validateType(undefined)).toEqual(false);
+	expect(dateValidator.validateType(null)).toEqual(false);
+	expect(dateValidator.validateType('str')).toEqual(false);
+	expect(dateValidator.validateType('')).toEqual(false);
+	expect(dateValidator.validateType({})).toEqual(false);
+	expect(dateValidator.validateType([])).toEqual(false);
+});
+
+test('Schema validator with unknown props throws', () => {
+	const stringSchema = {
+		type: Boolean,
+		required: true,
+		banana: 'banana?'
+	};
+
+	function validate() {
+		dateValidator.validateSchema(stringSchema);
+	}
+
+	expect(validate).toThrowError('Unknown property "banana"');
+});
+
+test("Schema validator doesn't throw when all fields are valid", () => {
+	const stringSchema = {
+		type: Boolean,
+		required: true
+	};
+
+	function validate() {
+		dateValidator.validateSchema(stringSchema);
+	}
+
+	expect(validate).not.toThrow();
+});
