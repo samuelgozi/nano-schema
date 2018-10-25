95b749a7ff8e4103b15f8796a881119277d95424
Samuel Elgozi <samu.elgozi@gmail.com>
2018-10-25 17:26:22 +0300

    Array schemas now work

--- src/index.js
+++ src/index.js
@@ -31,71 +31,91 @@ class Schema {
 	 * Validates that a schema is formatted correctly.
 	 */
 	validateSchema(schema = this.__schema) {
-		for (const propName in schema) {
-			const prop = schema[propName];
+		for (const fieldName in schema) {
+			const prop = schema[fieldName];
 			const typeValidator = validators.get(prop.type);
+
+			if (typeValidator === undefined) {
+				throw Error(`Invalid type for the field "${fieldName}"`);
+			}
 
 			typeValidator.validateSchema(prop);
 		}
 	}
 
 	/*
-	 * Valdidates a given object against the schema.
+	 * Validate a value against a field schema.
+	 */
+	validateProp(value, schema, propName, parentProp) {
+		// Get the validator for the type of the schema.
+		const validator = validators.get(schema.type);
+
+		// Full path to the field, needed for easier debugging of nested schemas.
+		const propPath =
+			parentProp !== undefined ? parentProp + '.' + propName : propName;
+
+		// If the field is required, validate that its not empty.
+		if (schema.required === true) {
+			// If the schema validator provided a specific
+			// method for checking required fields, then use it.
+			const validateRequired =
+				validator.required !== undefined
+					? validator.required
+					: validator.validateType;
+
+			// Bind the function to the validator object to allow `this` access.
+			const fieldIsEmpty = !validateRequired.call(validator, value);
+
+			if (fieldIsEmpty) {
+				throw Error(`The field "${propPath}" is required`);
+			}
+		}
+
+		// Check the the value matches an 'enum' when enum is specified.
+		if (schema.enum !== undefined) {
+			if (!schema.enum.includes(value)) {
+				throw Error(
+					`The field "${propPath}" can only be one of: ${schema.enum.join(
+						', '
+					)}`
+				);
+			}
+		}
+
+		// If the field is not undefined, then validate its type.
+		// We do this check because we are iterating over the schema, not the object itself.
+		if (value !== undefined) {
+			const isValid = validator.validateType(value);
+
+			if (!isValid) {
+				throw Error(`The field "${propPath}" is not of the correct type`);
+			}
+		}
+
+		// If the field is an object then recursively run the validator on it.
+		if (schema.type === Object) {
+			this.validate(value, schema.child, propName);
+		}
+
+		if (schema.type === Array) {
+			this.validateArray(value, schema, propName);
+		}
+	}
+
+	/*
+	 * Valdidates an object and all of its fields against a schema.
 	 */
 	validate(object, schema = this.__schema, fieldParent) {
 		// Used to track which props were validated.
 		const objectProps = new Set(Object.keys(object));
 
-		// Loop over the keys in the schema(on it the object to validate).
+		// Loop over the keys in the schema(no over the object to validate).
 		for (const fieldName in schema) {
 			const fieldValue = object[fieldName];
 			const fieldSchema = schema[fieldName];
-			const validator = validators.get(fieldSchema.type);
-			// Full path to the field, needed for easied debugging of nested schemas.
-			const fieldPath =
-				fieldParent !== undefined ? fieldParent + '.' + fieldName : fieldName;
 
-			// If the field is required, validate that its not empty.
-			if (fieldSchema.required === true) {
-				// If the schema validator provided a specific
-				// method for checking required fields, then use it.
-				const validateRequired =
-					validator.required !== undefined
-						? validator.required
-						: validator.validateType;
-
-				// Bind the function to the validator object to allow `this` access.
-				const fieldIsEmpty = !validateRequired.call(validator, fieldValue);
-
-				if (fieldIsEmpty) {
-					throw Error(`The field "${fieldPath}" is required`);
-				}
-			}
-
-			// Check the the value matches an 'enum' when enum is declared.
-			if (fieldSchema.enum !== undefined) {
-				if (!fieldSchema.enum.includes(fieldValue)) {
-					throw Error(
-						`The field "${fieldPath}" can only be one of: ${fieldSchema.enum.join(
-							', '
-						)}`
-					);
-				}
-			}
-
-			// If the field is not empty, then validate its type.
-			if (fieldValue !== undefined) {
-				const isValid = validator.validateType(fieldValue);
-
-				if (!isValid) {
-					throw Error(`The field "${fieldPath}" is not of the correct type`);
-				}
-			}
-
-			// If the field is an object then recursively run the validator on it.
-			if (fieldSchema.type === Object) {
-				this.validate(fieldValue, fieldSchema.child, fieldName);
-			}
+			// Validate the prop agains its field schema.
+			this.validateProp(fieldValue, fieldSchema, fieldName, fieldParent);
 
 			// Remove the prop from the list of properties left in the object.
 			objectProps.delete(fieldName);
@@ -114,6 +134,54 @@ class Schema {
 
 			throw Error(
 				'The object contains invalid properties: ' + invalidProps.join(', ')
+			);
+		}
+	}
+
+	/*
+	 * Validates that an array of values matches
+	 * at least one schema in an array of schemas.
+	 */
+	// TODO: refactor the code to be simpler.
+	validateArray(array, schema, propParent) {
+		// Validate the inputs.
+		if (!Array.isArray(array))
+			throw Error(
+				`Array validator needs the first argument to be an array, instead received "${typeof array}"`
+			);
+
+		if (schema === undefined)
+			throw Error(
+				`Array validator needs the second argument to be the array schema, instead received "${typeof schema}"`
+			);
+
+		for (const index in array) {
+			const value = array[index];
+			const propName = `[${index}]`;
+			let propItemMatched = false;
+
+			for (const fieldSchema of schema.child) {
+				try {
+					this.validateProp(value, fieldSchema, propName, propParent);
+
+					// If the validation didn't throw then it means
+					// that the value matched the schema, we can break and move into the next array value.
+					// Update the 'propItemMatched' to tell the outer loop to move into the next value.
+					propItemMatched = true;
+					break;
+				} catch (e) {
+					// Else if the validation throwed, then we need to keep checking.
+					continue;
+				}
+			}
+
+			// If the current propert matched a valid schema, then move into the next prop.
+			if (propItemMatched) continue;
+
+			// If no validation worked(the function would've
+			// returned, and never get here), Then throw an error.
+			throw Error(
+				`The property "${propParent + propName}" is not of the correct type`
 			);
 		}
 	}
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,11 +1,39 @@
 const Schema = require('../src/index');
 
-describe("Creating a schema with correct syntax doesn't throw", () => {
+describe('Creating a schema with correct syntax', () => {
 	test('Verbose schema syntax', () => {
 		const schemaObject = {
-			username: {
+			gender: {
 				type: String,
+				required: true,
+				enum: ['male', 'female']
+			},
+			age: {
+				type: Number,
 				required: true
+			},
+			married: {
+				type: Boolean
+			},
+			birthdate: {
+				type: Date
+			},
+			address: {
+				type: Object,
+				required: false,
+				child: {
+					city: {
+						type: String,
+						required: true
+					},
+					street: {
+						type: String,
+						required: true
+					}
+				}
+			},
+			favoriteMovies: {
+				type: Array
 			}
 		};
 
@@ -16,18 +44,6 @@ describe("Creating a schema with correct syntax doesn't throw", () => {
 		expect(createNewSchema).not.toThrow();
 	});
 
-	// test('Shortcut schema syntax', () => {
-	// 	const schemaObject = {
-	// 		username: String
-	// 	};
-
-	// 	function createNewSchema() {
-	// 		new Schema(schemaObject);
-	// 	}
-
-	// 	expect(createNewSchema).not.toThrow();
-	// });
-});
 
 test('Throws when creating a schema with anything other than an object', () => {
 	function createNewSchema() {
@@ -37,47 +53,39 @@ test('Throws when creating a schema with anything other than an object', () => {
 	expect(createNewSchema).toThrow('Schema must be an object');
 });
 
-test("Doesn't throw when validating an object that matches the schema", () => {
-	function createNewSchema() {
-		const schema = new Schema({
-			username: {
-				type: String
-			},
-			age: {
-				type: Number
+	test('Verbose array schema syntax', () => {
+		const schemaObject = {
+			arrayOfStuff: {
+				type: Array,
+				child: [
+					{
+						type: String
+					},
+					{
+						type: Number
+					}
+				]
 			}
-		});
+		};
 
-		schema.validate({
-			username: 'samuel',
-			age: 24
-		});
-	}
+		function createNewSchema() {
+			new Schema(schemaObject);
+		}
 
-	expect(createNewSchema).not.toThrow();
+		expect(createNewSchema).not.toThrow();
+	});
 });
 
-test('Throws when the object contains properties not specified in the schema', () => {
-	function createNewSchema() {
-		const schema = new Schema({
-			username: {
-				type: String
-			}
-		});
+describe('Creating a schema with incorrect syntax', () => {
+	test('Instantiating a `new Schema` with an argument that is not an object', () => {
+		function createNewSchema() {
+			new Schema(() => {});
+		}
 
-		schema.validate({
-			username: 'samuel',
-			password: '1234'
-		});
-	}
+		expect(createNewSchema).toThrow('Schema must be an object');
+	});
 
-	expect(createNewSchema).toThrow(
-		'The object contains invalid properties: password'
-	);
-});
-
-describe('Throws when creating a schema with invalid field options', () => {
-	test('Invalid option in "String" type', () => {
+	test('Creating a schema with invalid field options', () => {
 		function createNewSchema() {
 			new Schema({
 				username: {
@@ -89,125 +97,48 @@ describe('Throws when creating a schema with invalid field options', () => {
 
 		expect(createNewSchema).toThrow('Unknown property "name"');
 	});
-});
 
-describe('Throws when the type of the property is not correct', () => {
-	test('String', () => {
-		function testSchema() {
-			const schema = new Schema({
-				name: {
-					type: String
-				}
-			});
+	test('Creating a schema with a field that has an invalid type', () => {
+		const schemaObject = {
+			imVeryCool: false
+		};
 
-			schema.validate({
-				name: 42
-			});
+		function createNewSchema() {
+			new Schema(schemaObject);
 		}
 
-		expect(testSchema).toThrow('The field "name" is not of the correct type');
+		expect(createNewSchema).toThrow('Invalid type for the field "imVeryCool"');
 	});
+});
 
-	test('Number', () => {
-		function testSchema() {
+describe('Validating objects that match the schema', () => {
+	test('Schema with only a type', () => {
+		function createNewSchema() {
 			const schema = new Schema({
+				username: {
+					type: String
+				},
 				age: {
 					type: Number
 				}
 			});
 
 			schema.validate({
-				age: 'Forty Two'
+				username: 'samuel',
+				age: 24
 			});
 		}
 
-		expect(testSchema).toThrow('The field "age" is not of the correct type');
+		expect(createNewSchema).not.toThrow();
 	});
 
-	test('Boolean', () => {
-		function testSchema() {
-			const schema = new Schema({
-				likesPancakes: {
-					type: Boolean
-				}
-			});
-
-			schema.validate({
-				likesPancakes: 'Of course!'
-			});
-		}
-
-		expect(testSchema).toThrow(
-			'The field "likesPancakes" is not of the correct type'
-		);
-	});
-});
-
-test("Throw's when a required field is empty", () => {
-	function createNewSchema() {
-		const schema = new Schema({
-			username: {
-				type: String,
-				required: true
-			},
-			age: {
-				type: Number
-			}
-		});
-
-		schema.validate({
-			age: 24
-		});
-	}
-
-	expect(createNewSchema).toThrow('The field "username" is required');
-});
-
-test("Correct values with 'enum' don't throw", () => {
-	function createNewSchema() {
-		const schema = new Schema({
-			gender: {
-				type: String,
-				enum: ['male', 'female']
-			},
-			favoriteNumber: {
-				type: Number,
-				enum: [42, 84]
-			}
-		});
-
-		schema.validate({
-			gender: 'male',
-			favoriteNumber: 42
-		});
-	}
-
-	expect(createNewSchema).not.toThrow();
-});
-
-describe("Throw when 'enum' is specified, but the value isn't any of the allowed options", () => {
-	test('String Type', () => {
+	test("Correct values with 'enum'", () => {
 		function createNewSchema() {
 			const schema = new Schema({
 				gender: {
 					type: String,
 					enum: ['male', 'female']
-				}
-			});
-
-			schema.validate({
-				gender: 'neutral'
-			});
-		}
-
-		expect(createNewSchema).toThrow(
-			'The field "gender" can only be one of: male, female'
-		);
-	});
-
-	test('Number Type', () => {
-		function createNewSchema() {
-			const schema = new Schema({
+				},
 				favoriteNumber: {
 					type: Number,
 					enum: [42, 84]
@@ -215,39 +146,261 @@ describe("Throw when 'enum' is specified, but the value isn't any of the allowed
 			});
 
 			schema.validate({
-				favoriteNumber: 34
+				gender: 'male',
+				favoriteNumber: 42
 			});
 		}
 
-		expect(createNewSchema).toThrow(
-			'The field "favoriteNumber" can only be one of: 42, 84'
-		);
+		expect(createNewSchema).not.toThrow();
 	});
 });
 
-describe('Nested schemas', () => {
-	test("Throws when the nested object doens't match the schema", () => {
+describe("Validating object that dons't match a schema", () => {
+	test('Object contains properties not specified in the schema', () => {
 		function createNewSchema() {
 			const schema = new Schema({
-				address: {
-					type: Object,
-					child: {
-						city: {
-							type: String
-						}
-					}
+				username: {
+					type: String
 				}
 			});
 
 			schema.validate({
-				address: {
-					city: 42
-				}
+				username: 'samuel',
+				password: '1234'
 			});
 		}
 
 		expect(createNewSchema).toThrow(
-			'The field "address.city" is not of the correct type'
+			'The object contains invalid properties: password'
+		);
+	});
+
+	describe('Properties are of different type that in the schema', () => {
+		test('Incorrect value for a String field', () => {
+			function testSchema() {
+				const schema = new Schema({
+					name: {
+						type: String
+					}
+				});
+
+				schema.validate({
+					name: 42
+				});
+			}
+
+			expect(testSchema).toThrow('The field "name" is not of the correct type');
+		});
+
+		test('Incorrect value for a  Number field', () => {
+			function testSchema() {
+				const schema = new Schema({
+					age: {
+						type: Number
+					}
+				});
+
+				schema.validate({
+					age: 'Forty Two'
+				});
+			}
+
+			expect(testSchema).toThrow('The field "age" is not of the correct type');
+		});
+
+		test('Incorrect value for a Boolean field', () => {
+			function testSchema() {
+				const schema = new Schema({
+					likesPancakes: {
+						type: Boolean
+					}
+				});
+
+				schema.validate({
+					likesPancakes: 'Of course!'
+				});
+			}
+
+			expect(testSchema).toThrow(
+				'The field "likesPancakes" is not of the correct type'
+			);
+		});
+	});
+
+	describe("Throw when 'enum' is specified, but the value isn't any of the allowed options", () => {
+		test('String Type', () => {
+			function createNewSchema() {
+				const schema = new Schema({
+					gender: {
+						type: String,
+						enum: ['male', 'female']
+					}
+				});
+
+				schema.validate({
+					gender: 'neutral'
+				});
+			}
+
+			expect(createNewSchema).toThrow(
+				'The field "gender" can only be one of: male, female'
+			);
+		});
+
+		test('Number Type', () => {
+			function createNewSchema() {
+				const schema = new Schema({
+					favoriteNumber: {
+						type: Number,
+						enum: [42, 84]
+					}
+				});
+
+				schema.validate({
+					favoriteNumber: 34
+				});
+			}
+
+			expect(createNewSchema).toThrow(
+				'The field "favoriteNumber" can only be one of: 42, 84'
+			);
+		});
+	});
+
+	test("Throw's when a required field is empty", () => {
+		function createNewSchema() {
+			const schema = new Schema({
+				username: {
+					type: String,
+					required: true
+				},
+				age: {
+					type: Number
+				}
+			});
+
+			schema.validate({
+				age: 24
+			});
+		}
+
+		expect(createNewSchema).toThrow('The field "username" is required');
+	});
+});
+
+test("Nested object doens't match the schema", () => {
+	function createNewSchema() {
+		const schema = new Schema({
+			address: {
+				type: Object,
+				child: {
+					city: {
+						type: String
+					}
+				}
+			}
+		});
+
+		schema.validate({
+			address: {
+				city: 42
+			}
+		});
+	}
+
+	expect(createNewSchema).toThrow(
+		'The field "address.city" is not of the correct type'
+	);
+});
+
+describe('Validating array fields', () => {
+	test('Value matches the array schema', () => {
+		const arraySchema = new Schema({
+			arrayOfStuff: {
+				type: Array,
+				child: [
+					{
+						type: String
+					},
+					{
+						type: Number
+					}
+				]
+			}
+		});
+
+		function validateArrayWithString() {
+			arraySchema.validate({
+				arrayOfStuff: ['Hi there!']
+			});
+		}
+
+		function validateArrayWithNumber() {
+			arraySchema.validate({
+				arrayOfStuff: [42]
+			});
+		}
+
+		function validateArrayWithBoth() {
+			arraySchema.validate({
+				arrayOfStuff: ['Hi there!', 42]
+			});
+		}
+
+		expect(validateArrayWithString).not.toThrow();
+		expect(validateArrayWithNumber).not.toThrow();
+		expect(validateArrayWithBoth).not.toThrow();
+	});
+
+	test("Value doesn't match the array schema", () => {
+		const arraySchema = new Schema({
+			arrayOfStuff: {
+				type: Array,
+				child: [
+					{
+						type: String
+					},
+					{
+						type: Number
+					}
+				]
+			}
+		});
+
+		function validateSchema() {
+			arraySchema.validate({
+				arrayOfStuff: [false]
+			});
+		}
+
+		expect(validateSchema).toThrow(
+			'The property "arrayOfStuff[0]" is not of the correct type'
+		);
+	});
+
+	test('Combined props with allowed and invalid types throws at the right prop', () => {
+		const arraySchema = new Schema({
+			arrayOfStuff: {
+				type: Array,
+				child: [
+					{
+						type: String
+					},
+					{
+						type: Number
+					}
+				]
+			}
+		});
+
+		function validateSchema() {
+			arraySchema.validate({
+				arrayOfStuff: ['Samuel', 42, false]
+			});
+		}
+
+		expect(validateSchema).toThrow(
+			'The property "arrayOfStuff[2]" is not of the correct type'
 		);
 	});
 });
