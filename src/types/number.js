2b760614424dd580faa34476d0dbbf4efefe705c
Samuel Elgozi <samu.elgozi@gmail.com>
2019-09-01 23:45:28 +0300

    Fix: use parseFloar instead of parseint in Number validator

--- src/types/number.js
+++ src/types/number.js
@@ -2,7 +2,7 @@ module.exports = {
 	allowedProps: ['required', 'enum', 'coerce'],
 
 	validateType(value, schema = {}) {
-		if (schema.coerce === true) value = parseInt(value);
+		if (schema.coerce === true) value = parseFloat(value);
 		return typeof value === 'number' && !Number.isNaN(value);
 	}
 };
