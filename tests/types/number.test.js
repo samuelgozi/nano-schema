66fabc617530cafb7575e472de943ec390b922cc
Samuel Elgozi <samu.elgozi@gmail.com>
2019-09-01 20:12:47 +0300

    fix: Number coercion only works when the string is literaly a number

--- tests/types/number.test.js
+++ tests/types/number.test.js
@@ -20,12 +20,16 @@ test('Correctly validates numbers in strings when "coerce" is active', () => {
 	expect(numberValidator.validateType('42', { coerce: true })).toEqual(true);
 
 	// Should fail
-	expect(numberValidator.validateType(function() {})).toEqual(false);
-	expect(numberValidator.validateType(undefined)).toEqual(false);
-	expect(numberValidator.validateType(null)).toEqual(false);
-	expect(numberValidator.validateType(true)).toEqual(false);
-	expect(numberValidator.validateType('str')).toEqual(false);
-	expect(numberValidator.validateType('')).toEqual(false);
-	expect(numberValidator.validateType({})).toEqual(false);
-	expect(numberValidator.validateType([])).toEqual(false);
+	expect(numberValidator.validateType(function() {}, { coerce: true })).toEqual(
+		false
+	);
+	expect(numberValidator.validateType(undefined, { coerce: true })).toEqual(
+		false
+	);
+	expect(numberValidator.validateType(null, { coerce: true })).toEqual(false);
+	expect(numberValidator.validateType(true, { coerce: true })).toEqual(false);
+	expect(numberValidator.validateType('str', { coerce: true })).toEqual(false);
+	expect(numberValidator.validateType('', { coerce: true })).toEqual(false);
+	expect(numberValidator.validateType({}, { coerce: true })).toEqual(false);
+	expect(numberValidator.validateType([], { coerce: true })).toEqual(false);
 });
